ce3661b7c244f1674fde110a769332fff0124f7a
abhisheksharma <abhishesk.s@flitwebs.com>
2020-12-15 18:30:37 +0530

    one message is added

---
+++ src/main/java/com/example/springwithgit/SpringUploadToGitApplication.java
@@ -0,0 +1,16 @@
+package com.example.springwithgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUploadToGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringUploadToGitApplication.class, args);
+        System.out.println("This Spring Project is uploaded using idea IDE");
+
+        System.out.println("This if for demo purpose");
+    }
+
+}
